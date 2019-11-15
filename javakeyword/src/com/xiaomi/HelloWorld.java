0b1134ec9d5d05b4b15358c25b0b4bc2dc3ebd50
duyiying@duyiyinggit config --global user.name duyiying@duyiying <duyiying@xiaomi.com>
2019-11-15 12:46:39 +0800

    HelloWorld code

---
+++ javakeyword/src/com/xiaomi/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.xiaomi;
+/*
+ *項目名: javakeyword
+ *文件名: HelloWorld
+ *創建者: yim
+ *創建時間: 2019/11/15 11:42
+ *描述: TODO
+
+ */
+
+public class HelloWorld {
+
+    private String name = "Hello World!";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ javakeyword/src/com/xiaomi/Main.java
@@ -0,0 +1,11 @@
+package com.xiaomi;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        HelloWorld hello = new HelloWorld();
+        System.out.println(hello.getName());
+
+    }
+}
